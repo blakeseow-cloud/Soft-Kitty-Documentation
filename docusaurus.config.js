435f0498ab17c7af35581d3d8a95a5651e89872d
Blake Seow <blake_seow@hotmail.com>
2026-02-24 20:21:12 +0800

    Update docs

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,10 +20,10 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://blakeseow-cloud.github.io',
+  url: 'https://www.soft-kitty.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/Soft-Kitty-Documentation/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
@@ -103,7 +103,7 @@ const config = {
             items: [
               {
                 label: 'User Manual',
-                to: '/docs/intro',
+                to: '/docs/soft-kitty/overview',
               },
             ],
           },
